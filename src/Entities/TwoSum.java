9ba977016674d49a338cbf1cdce1b24cdc243f8a
JunHokama <ejunhok@gmail.com>
2023-10-13 13:34:06 -0300

    refactor: formatacao de linhas

--- src/Entities/TwoSum.java
+++ src/Entities/TwoSum.java
@@ -1,14 +1,14 @@
 package Entities;
 public class TwoSum {
 	
-	public int [] twoSum(int []arr, int target) {
+public int [] twoSum(int []arr, int target) {
 		
-		for(int i = 0; i<arr.length;i++) {
-			for(int j = 0; j<arr.length;j++) {
-				if(arr[i] + arr[j] == target) {
-					return new int[] {i,j};
-				}
+	for(int i = 0; i<arr.length;i++) {
+		for(int j = 0; j<arr.length;j++) {
+			if(arr[i] + arr[j] == target) {
+				return new int[] {i,j};
 			}
 		}
-		return arr;}
 	}
+	return arr;}
+}
